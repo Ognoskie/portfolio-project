bcf84fb9890525ab756b11cc6b2a4ef11341928f
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-08 18:01:36 -0500

    added home folder and home js file and social js file

---
+++ src/components/home/Social.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Social = () => {
+    return (
+        <div className="home_social">
+            <a href="" className="home_social-icon" target="_blank"></a>
+
+            <a href="" className="home_social-icon" target="_blank"></a>
+
+            <a href="" className="home_social-icon" target="_blank"></a>
+        </div>
+    )
+}
+
+export default Social;
