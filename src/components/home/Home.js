d492cbc3a7230848d1fd3dcb0cb3c3809cc6929f
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-10 15:18:08 -0500

    added title home and svg

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import Social from "./Social";
+import "home.css";
+import Data from "./Data";
 const Home = () => {
     return (
       <section className="home section" id="home">
           <div className="home_container container grid">
               <div className="home_content grid">
-                  
+                <Social />
+
+                  <div className="home_img"></div>
+
+                  <Data />
               </div>
           </div>
       </section>
