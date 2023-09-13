3619b71554f6132b64a1f2fcedd2753aea388826
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-13 11:37:19 -0500

    finished about me

--- src/components/about/Info.js
+++ src/components/about/Info.js
@@ -4,16 +4,22 @@ const Info = () => {
     return(
         <div className="about_info grid">
             <div className="about_box">
+                <i className='bx bx-award about_icon'></i>
+
                 <h3 className="about_title">Experience</h3>
-                <span className="about_subtitle">680+ hours of active coding in a web development bootcamp</span>
+                <span className="about_subtitle">680+ hours of active coding</span>
             </div>
 
             <div className="about_box">
+                <i className='bx bxs-briefcase-alt about_icon'></i>
+
                 <h3 className="about_title">Completed</h3>
                 <span className="about_subtitle">10+ Projects</span>
             </div>
 
             <div className="about_box">
+                <i className='bx bx-code about_icon'></i>
+
                 <h3 className="about_title">Languages</h3>
                 <span className="about_subtitle">8</span>
             </div>
