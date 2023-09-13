3f035f4de11058e87b95f54eaf411e2ecfe58fbf
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-13 11:51:38 -0500

    added skills file / frontend file / backend file

---
+++ src/components/skills/Frontend.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Frontend = () => {
+    return (
+        <div className="skills_contemnt">
+            <h3 className="skills_title"></h3>
+
+        </div>
+    )
+}
+
+export default Frontend;
