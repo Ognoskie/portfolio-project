8be012111cc1049f95f5ef9f5cfc9ed84691ef7b
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-15 16:12:07 -0500

    added services css and modal using react

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
+import "./services.css";
 
 const Services = () => {
+        const [toggleState, setToggleState] = useState(0);
+
+        const toggleTab = (index) => {
+            setToggleState(index);
+        }
+
     return (
         <section className="services section" id="services">
             <h2 className="section_title">Services</h2>
@@ -9,25 +16,165 @@ const Services = () => {
             <div className="services_container container grid">
                 <div className="services_content">
                     <div>
-                        <i className="uil uil services_icon"></i>
-                        <h3 className="services_title"></h3>
+                        <i className="uil uil-web-grid services_icon"></i>
+                        <h3 className="services_title">Front-end <br /> Developer </h3>
                     </div>
 
-                    <span className="services_button">View More <i
+                    <span className="services_button" onClick={() => toggleTab(1)}>View More<i
                         className="uil uil-arrow-right services_button-icon"></i></span>
 
-                    <div className="services_modal">
+                    <div className={toggleState === 1 ? "services_modal active-modal" : "services_modal"}>
                         <div className="services_modal-content">
-                            <i className="uil uil-times services_modal-close"></i>
+                            <i onClick={() => toggleTab(0)} className="uil uil-times services_modal-close"></i>
 
                             <h3 className="services_modal-title">Front-end Developer</h3>
-                            <p className="services_modal-description">Experienced in providing front-end web development to companies and clients.</p>
+                            <p className="services_modal-description">Ability to provide front-end web development to companies and clients.</p>
 
                             <ul className="services_modal-services grid">
-                                <li className="services_modal-services">
+                                <li className="services_modal-service">
                                     <i className="uil uil-check-circle services_modal-icon"></i>
                                     <p className="services_modal-info">
-                                        Website Development: Create and maintain websites, ensuring they are visually appealing, responsive, and functional across various devices and browsers.
+                                        Website Development
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Cross-Browser Compatibility
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Testing and Debugging
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Performance Optimization
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Front-End Frameworks
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="services_content">
+                    <div>
+                        <i className="uil uil-arrow services_icon"></i>
+                        <h3 className="services_title">Back-end <br /> Developer</h3>
+                    </div>
+
+                    <span onClick={() => toggleTab(2)} className="services_button">View More<i
+                        className="uil uil-arrow-right services_button-icon"></i></span>
+
+                    <div className={toggleState === 2 ? "services_modal active-modal" : "services_modal"}>
+                        <div className="services_modal-content">
+                            <i onClick={() => toggleTab(0)}  className="uil uil-times services_modal-close"></i>
+
+                            <h3 className="services_modal-title">Back-end Developer</h3>
+                            <p className="services_modal-description">Ability to provide back-end web development to companies and clients.</p>
+
+                            <ul className="services_modal-services grid">
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Server Development
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Database Management
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Authentication and Authorization
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Performance Optimization
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Error Handling and Logging
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="services_content">
+                    <div>
+                        <i className="uil uil-edit services_icon"></i>
+                        <h3 className="services_title">UX/UI <br /> Designer</h3>
+                    </div>
+
+                    <span onClick={() => toggleTab(3)}  className="services_button">View More<i
+                        className="uil uil-arrow-right services_button-icon"></i></span>
+
+                    <div className={toggleState === 3 ? "services_modal active-modal" : "services_modal"}>
+                        <div className="services_modal-content">
+                            <i onClick={() => toggleTab(0)} className="uil uil-times services_modal-close"></i>
+
+                            <h3 className="services_modal-title">UX/UI Designer</h3>
+                            <p className="services_modal-description">Ability to provide UX/UI design to companies and clients.</p>
+
+                            <ul className="services_modal-services grid">
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        UX/UI Design
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        User Research
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Wire-framing
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        Visual Design
+                                    </p>
+                                </li>
+
+                                <li className="services_modal-service">
+                                    <i className="uil uil-check-circle services_modal-icon"></i>
+                                    <p className="services_modal-info">
+                                        User Flows
                                     </p>
                                 </li>
                             </ul>
