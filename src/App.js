ffa62d8ac8a2dd548f8226ee5f7803b3cd98a93c
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-16 17:19:45 -0500

    imported work to app js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
+import Work from "./components/work/Work";
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
                 <About />
                 <Skills />
                 <Services />
+                <Work />
 
             </main>
         </>
