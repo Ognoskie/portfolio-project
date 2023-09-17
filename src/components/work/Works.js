7cab1c56afc8fdfee6906ffc67002bd473b614ce
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-17 12:55:19 -0500

    added more to works

--- src/components/work/Works.js
+++ src/components/work/Works.js
@@ -1,15 +1,20 @@
 import React from "react";
 import {projectsData} from "./Data";
 import {projectsNav} from "./Data";
+import {findAllByDisplayValue} from "@testing-library/react";
 
 const Works = () => {
     return (
-        <div className="work_filters">
-            {projectsNav.map((item, index) => {
-                return (
-                    <span className="work_item">{item.name}</span>
-                )
-            })}
+        <div>
+            <div className="work_filters">
+                {projectsNav.map((item, index) => {
+                    return <span className="work_item" key={index}>{item.name}</span>
+                })}
+            </div>
+
+            <div className="work_container container grid">
+                {}
+            </div>
         </div>
     )
 }
