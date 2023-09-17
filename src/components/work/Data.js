8f0736451e62497cde37e87a9ad5e60859a0f0f2
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-16 20:06:48 -0500

    added more ti works js

---
+++ src/components/work/Data.js
@@ -0,0 +1,38 @@
+import Work1 from "../../assets/work1.jpg"
+import Work2 from "../../assets/work2.jpg"
+import Work5 from "../../assets/work5.jpg"
+
+
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title: 'Weather Map Project',
+        category: 'web',
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: 'Movie App Project',
+        category: 'app',
+    },
+    {
+        id: 3,
+        image: Work5,
+        title: 'Dev Lister Project',
+        category: 'web',
+    },
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'web',
+    },
+    {
+        name: 'app',
+    },
+];
