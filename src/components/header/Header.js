cf6559368c90e00f2a94479413a1881178506b0f
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-19 14:58:27 -0500

    changed name title href form index.html to #home

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
     return (
         <header className="header">
             <nav className="nav container">
-                <a href="index.html" className="nav_logo">Ognoskie</a>
+                <a href="#home" className="nav_logo">Ognoskie</a>
 
                 <div className={Toggle ? "nav_menu show-menu" : "nav_menu"}>
                     <ul className="nav_list grid">
