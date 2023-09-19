ea45ffb4c3d899274a3ae04bfc9d37f930bece43
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-19 12:04:52 -0500

    finished contact part of portfolio

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
 
             <div className="contact_container container grid">
                 <div className="contact_content">
-                    <h3 className="contact_titile">Contact Info</h3>
+                    <h3 className="contact_title">Contact Info</h3>
 
                     <div className="contact_info">
                         <div className="contact_card">
@@ -18,7 +18,8 @@ const Contact = () => {
                             <h3 className="contact_card-title">Email</h3>
                             <span className="contact_card-data">bradenjognoskie@gmail.com</span>
 
-                            <a href="mailto:bradenjognoskie@gmail.com.com" className="contact_button"> Email me
+                            <a href="mailto:bradenjognoskie@gmail.com.com" target="_blank" className="contact_button">
+                                Email me
                                 <i className="bx bx-right-arrow-alt contact_button-icon"></i>
                             </a>
                         </div>
@@ -29,16 +30,20 @@ const Contact = () => {
                             <h3 className="contact_card-title">Cell Phone</h3>
                             <span className="contact_card-data">(210) 500-0386</span>
 
-                            <a className="contact_button">Call me</a>
+                            <a className="contact_button">
+                                Call me
+                            </a>
                         </div>
 
                         <div className="contact_card">
                             <i className="bx bx-message contact_card-icon"></i>
 
-                            <h3 className="contact_card-title">Message me</h3>
+                            <h3 className="contact_card-title">Message</h3>
                             <span className="contact_card-data">(210) 500-0386</span>
 
-                            <a className="contact_button">Text me</a>
+                            <a className="contact_button">
+                                Text me
+                            </a>
                         </div>
                     </div>
                 </div>
