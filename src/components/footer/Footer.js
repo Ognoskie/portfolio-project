f7e2686e2cda4912cccd48b6223ad1ab81730e36
Braden Wittkop <bradenjwittkop@gmail.com>
2023-09-19 13:18:05 -0500

    fixed spacing

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,25 @@ import "./footer.css";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className="footer">
+            <div className="footer_container container">
+                <h1 className="footer_title">Ognoskie</h1>
+                
+                <ul className="footer_list">
+                    <li>
+                        <a href="#about" className="footer_link">About</a>
+                    </li>
+
+                    <li>
+                        <a href="#portfolio" className="footer_link">Projects</a>
+                    </li>
+
+                    <li>
+                        <a href="#" className="footer_link"></a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
     )
 }
 
